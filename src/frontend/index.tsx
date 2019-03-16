1edd58cfa79189ae66e1494d24df73adb5286193
Amaimersion <amatsukine@gmail.com>
2019-03-16 18:39:28 +0300

    Add basic index files

---
+++ src/frontend/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+function App(): JSX.Element {
+    return (
+        <h1>Hello, World!</h1>
+    );
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
